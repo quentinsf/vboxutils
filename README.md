632f3451ff4cd667fe7cd7f456c34233378d309c
Quentin Stafford-Fraser <quentin@pobox.com>
2015-07-16 12:42:00 +0100

    Refactor, restructure, document

--- README.md
+++ README.md
@@ -1,6 +1,34 @@
 # VBOX experiments
 
-# Mac OS X install
+The `vboxread.py` script can read the .VBO format files produced by a RaceLogic VBOX and perform a few operations on the data.
+
+It will perform a couple of conversions:
+
+* The time, which is in the VBOX file as HHMMSS.SSS, will be converted to absolute seconds since the epoch, assuming this time is on the same day as the creation time recorded in the file. This makes sense as long as a GPS is connected.
+
+* Latitude and longitude are in the VBOX file as minutes, with west being positive. This converts to degrees, with the more standard east as positive.
+
+
+## Basic use
+
+Run
+
+    ./vobxread.py --help
+
+for the options.  Filenames can generally be specified as '-' if you want to use stdin/stdout. You can also specify more than one option at a time, for example:
+
+    ./vboxread.py --graph --csv my.csv my.vbo
+
+This will plot a graph and output a CSV file of the data.
+
+
+## Installation
+
+This depends on some other python packages, so you need to install them.
+
+    pip install -r requirements.txt
+
+## Mac OS X install
 
 On a Mac, you can use the built-in matplotlib and numpy or one of the binary installers, but if you want to run entirely in a virtualenv, you'll need to follow more complex instructions such as these to build matplotlib etc:
 
@@ -12,7 +40,8 @@ A simpler alternative is to build your virtualenv allowing access to the system 
     . env/bin/activate
     pip install -r requirements.txt
 
-# VBO format
+
+## VBO format
 
 Some information available [here][1].
 
--- vboxread.py
+++ vboxread.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 #
-# Run this, passing the VBO filename as an argument
+# Run this, passing the VBO filename as an argument.
+# Specify one of the options to make it do something.
+# --help will give you the details.
 #
 
 import collections
@@ -10,8 +12,7 @@ import sys
 import click
 import colorama
 from datetime import datetime, timedelta
-from numpy import *
-import matplotlib.pyplot as plt
+
 
 class VBoxData:
     """
@@ -32,104 +33,107 @@ class VBoxData:
         self.headers = []
         self.column_names = []
         self.data = []
-
         if from_vbo_file:
-            # Crude parser
-            section = None
-            for raw_line in from_vbo_file:
-                line = raw_line.strip()
-
-                # Start of new section
-                m = re.match(r'\[([\s\w)]+)\]', line)
-                if m is not None:
-                    section = m.group(1)
-
-                else:
-                    # We're within a section
-                                    
-                    if (section is None) and line.startswith('File created on'):
-                        # Will parse this at some point
-                        creation_date = line[16:]
-                        self.creation_date = datetime.strptime(creation_date, "%d/%m/%Y @ %H:%M:%S")
-                        self.creation_midnight = self.creation_date.replace(hour=0, minute=0, second=0, microsecond=0)
-
-                    if line and (section == 'header'):
-                        self.headers.append(line)
-
-                    if line and (section == 'comments'):
-                        self.comments.append(line)
-
-                    if line and (section == 'column names'):
-                        # To use the columns as field names in named tuples, we need to replace hyphens
-                        self.column_names = [c.replace('-','_') for c in line.split()]
-                        self.column_name_map = dict([k[::-1] for k in enumerate(self.column_names)])
-                        assert(self.column_names[1] == 'time')  # We assume this later
-                        assert(self.column_names[2] == 'lat')   # We assume this later
-                        assert(self.column_names[3] == 'long')  # We assume this later
-                        self.column_names.append('datetime')    # We'll add one of our own
-                        VBoxDataTuple = collections.namedtuple('VBoxDataTuple', self.column_names)
-
-                    if line and (section == 'data'):
-                        # I think data fields are always numbers, but in different formats
-                        # We'll treat them as floats for now
-                        bits = line.split()
-                        fields = [float(f) for f in bits]
-
-                        # Time, however, looks like a float but is HHMMSS.SS
-                        tstamp = bits[1]
-                        (hrs, mins, secs) = int(tstamp[0:2]), int(tstamp[2:4]), float(tstamp[4:])
-                        fields[1] = 3600 * hrs + 60 * mins + secs
-                        fields.append( self.creation_midnight.replace(hour = hrs, minute=mins, second=int(secs)) )
-
-                        # And lat and long are in minutes, with west as positive
-                        # Convert to conventional degrees
-                        fields[2] = float(fields[2])/60.0
-                        fields[3] = -1 * float(fields[3])/60.0
-
-                        # If there's no GPS signal, we won't have absolute time
-                        # We assume that time=000000.00 indicates the start of useful data
-                        if fields[1] == 0.0:
-                            self.data = []
-
-                        tup = VBoxDataTuple(*fields)
-                        self.data.append(tup)
-
-            self.min_lat = min([d.lat for d in self.data])
-            self.max_lat = max([d.lat for d in self.data])
-            self.min_long = min([d.long for d in self.data])
-            self.max_long = max([d.long for d in self.data])
+            self.read_vbo(from_vbo_file)
 
 
-@click.command()
-@click.option('--graph', '-g', default=False, is_flag=True, help="Draw a pretty plot")
-@click.option('--track', '-t', default=False, is_flag=True, help="Draw a map")
-@click.option('--gpx',   '-p', default=False, is_flag=True, help="Output a GPX file")
-@click.argument('vbo_file', type=click.File('r'))
-def main(graph, track, gpx, vbo_file):
-    v1 = VBoxData(vbo_file)
-    print >>sys.stderr, len(v1.data),"points"
-    # Dump as CSV
-    # csv_out =csv.writer(sys.stdout)
-    # csv_out.writerow(v1.column_names)
-    # for d in v1.data:
-    #     csv_out.writerow(d)
+    def read_vbo(self, from_vbo_file):
+        """
+        Populate this object by reading the specified VBO file.
 
-    # Playing with NumPy
-    # a = array(v1.data)
-    # b = a.transpose()
-    # plt.plot(b[v1.column_name_map['time']], b[v1.column_name_map['PedalPos_CH']])
-    # plt.plot(b[v1.column_name_map['time']], b[v1.column_name_map['BrakePressure_HS1_CH']])
-    # plt.plot(b[v1.column_name_map['time']], b[v1.column_name_map['VehicleSpeed_HS1_CH']])
-    # plt.show()
+        from_vbo_file should be an open file-like object.
+        """
+        section = None
+        for raw_line in from_vbo_file:
+            line = raw_line.strip()
 
-    if graph:
+            # Start of new section
+            m = re.match(r'\[([\s\w)]+)\]', line)
+            if m is not None:
+                section = m.group(1)
+
+            else:
+                # We're within a section
+                                
+                if (section is None) and line.startswith('File created on'):
+                    # Will parse this at some point
+                    creation_date = line[16:]
+                    self.creation_date = datetime.strptime(creation_date, "%d/%m/%Y @ %H:%M:%S")
+                    self.creation_midnight = self.creation_date.replace(hour=0, minute=0, second=0, microsecond=0)
+
+                if line and (section == 'header'):
+                    self.headers.append(line)
+
+                if line and (section == 'comments'):
+                    self.comments.append(line)
+
+                if line and (section == 'column names'):
+                    # To use the columns as field names in named tuples, we need to replace hyphens
+                    self.column_names = [c.replace('-','_') for c in line.split()]
+                    self.column_name_map = dict([k[::-1] for k in enumerate(self.column_names)])
+                    assert(self.column_names[1] == 'time')  # We assume this later
+                    assert(self.column_names[2] == 'lat')   # We assume this later
+                    assert(self.column_names[3] == 'long')  # We assume this later
+                    self.column_names.append('datetime')    # We'll add one of our own
+                    VBoxDataTuple = collections.namedtuple('VBoxDataTuple', self.column_names)
+
+                if line and (section == 'data'):
+                    # I think data fields are always numbers, but in different formats
+                    # We'll treat them as floats for now
+                    bits = line.split()
+                    fields = [float(f) for f in bits]
+
+                    # Time, however, looks like a float but is HHMMSS.SS
+                    tstamp = bits[1]
+                    (hrs, mins, secs) = int(tstamp[0:2]), int(tstamp[2:4]), float(tstamp[4:])
+                    fields[1] = 3600 * hrs + 60 * mins + secs
+                    fields.append( self.creation_midnight.replace(hour = hrs, minute=mins, second=int(secs)) )
+
+                    # And lat and long are in minutes, with west as positive
+                    # Convert to conventional degrees
+                    fields[2] = float(fields[2])/60.0
+                    fields[3] = -1 * float(fields[3])/60.0
+
+                    # If there's no GPS signal, we won't have absolute time
+                    # We assume that time=000000.00 indicates the start of useful data
+                    if fields[1] == 0.0:
+                        self.data = []
+
+                    tup = VBoxDataTuple(*fields)
+                    self.data.append(tup)
+
+        self.min_lat = min([d.lat for d in self.data])
+        self.max_lat = max([d.lat for d in self.data])
+        self.min_long = min([d.long for d in self.data])
+        self.max_long = max([d.long for d in self.data])
+
+
+    def write_csv(self, outfile=sys.stdout):
+        """
+        Create a CSV file of the data fields.
+
+        out_file should be an open file-like object.
+        """
+        csv_out =csv.writer(outfile)
+        csv_out.writerow(self.column_names)
+        for d in self.data:
+            csv_out.writerow(d)
+
+
+    def plot_graph(self):
+        """
+        Plot some interesting things on a graph.
+        """
+        # from numpy import *
+        import matplotlib.pyplot as plt
+
         plt.figure(1)
 
         plt.subplot(211)
         plt.title('Accelerator, brake and speed')
         accel_line, brake_line, speed_line = plt.plot(
-            [d.time for d in v1.data],   # x axis
-            [(d.PedalPos_CH, d.BrakePressure_HS1_CH, d.VehicleSpeed_HS1_CH ) for d in v1.data]  # y values
+            [d.time for d in self.data],   # x axis
+            [(d.PedalPos_CH, d.BrakePressure_HS1_CH, d.VehicleSpeed_HS1_CH ) for d in self.data]  # y values
         )
         plt.legend([accel_line, brake_line, speed_line], ['Accel', 'Brake', 'Speed'])
 
@@ -137,8 +141,8 @@ def main(graph, track, gpx, vbo_file):
         plt.title('Steering')
         plt.xlabel('Time (s)')
         steering_line, indicator_line = plt.plot(
-            [d.time for d in v1.data],   # x axis
-            [(d.SteeringWheelAngle_CH, [0, -100, 100][int(d.DirectionIndicationSwitchHS_CH)]) for d in v1.data]  # y values
+            [d.time for d in self.data],   # x axis
+            [(d.SteeringWheelAngle_CH, [0, -100, 100][int(d.DirectionIndicationSwitchHS_CH)]) for d in self.data]  # y values
         )
         plt.axhline()
         plt.ylabel('Deg left')
@@ -146,14 +150,24 @@ def main(graph, track, gpx, vbo_file):
 
         plt.show()
 
-    if track:
+
+    def plot_track(self):
+        """
+        Plot location and colour with speed.
+        """
+        import matplotlib.pyplot as plt
+
         plt.figure(2)
         plt.title('Track')
         ax = plt.gca()
         ax.set_axis_bgcolor((0,0,0))
-        max_vel = max([d.velocity for d in v1.data])
+        max_vel = max([d.velocity for d in self.data])
+        if max_vel == 0:
+            click.echo("No movement detected!", err=True)
+            sys.exit(1)
+
         prev_lat, prev_long = None, None
-        for d in v1.data[::10]:
+        for d in self.data[::10]:
             vel_norm = d.velocity/max_vel
             # lat and long appear to be in minutes N & W
             if prev_lat is not None:
@@ -162,7 +176,10 @@ def main(graph, track, gpx, vbo_file):
                     color=(vel_norm,0.4,1.0-vel_norm,1)
                 )
             prev_lat, prev_long = d.lat, d.long
-        # Coventry label    
+
+        plt.axis('equal')  # a degree is a degree
+        
+        # Coventry label
         plt.plot(-1.510948, 52.407762, marker='+', color='white') # Coventry
         plt.annotate('Coventry', xy=(-1.510948, 52.407762), xytext=(-1.509, 52.408), color='gray')
         # Warwick label
@@ -170,12 +187,39 @@ def main(graph, track, gpx, vbo_file):
         plt.annotate('Warwick Uni', xy=( -1.5626, 52.3838), xytext=( -1.5616, 52.3848,), color='gray')
         plt.show()
 
-    if gpx:
+
+    def write_gpx(self, outfile=sys.stdout):
         # Get the Jinja template for rendering a GPX file
         from jinja2 import Environment, FileSystemLoader
         env = Environment(loader=FileSystemLoader('.'))
         template = env.get_template('track.gpx.j2')
-        print template.render(vboxdata = v1)
+        outfile.write( template.render(vboxdata = self) )
+
+
+
+@click.command()
+@click.option('--graph', '-g', default=False, is_flag=True, help="Draw a pretty plot")
+@click.option('--track', '-t', default=False, is_flag=True, help="Draw a map")
+@click.option('--gpx',   '-p', default=None, type=click.File('w'), help="Output a GPX file")
+@click.option('--csv',   '-c', default=None, type=click.File('w'), help="Output a CSV file")
+@click.argument('vbo_file', type=click.File('r'))
+
+def main(graph, track, gpx, csv, vbo_file):
+    vbd = VBoxData(vbo_file)
+    print >>sys.stderr, len(vbd.data),"points"
+
+    if gpx:
+        vbd.write_gpx(gpx)
+
+    if csv:
+        vbd.write_csv(csv)
+
+    if graph:
+        vbd.plot_graph()
+
+    if track:
+        vbd.plot_track()
+
 
 if __name__ == '__main__':
     main()
